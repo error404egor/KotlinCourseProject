6f96b48e463a24cc1e3c30bf67ad68d3a6f7e553
error404egor <egoclash129@gmail.com>
2025-12-24 14:06:18 +0300

    first commit simple spring project

---
+++ src/main/kotlin/com/team322/prepovedia/PrepovediaApplication.kt
@@ -0,0 +1,11 @@
+package com.team322.prepovedia
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PrepovediaApplication
+
+fun main(args: Array<String>) {
+	runApplication<PrepovediaApplication>(*args)
+}
