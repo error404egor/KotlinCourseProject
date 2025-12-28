6131ed71ac268b190a921aa3cf594a312e9129b8
mamlik <sasamamleev1@gmail.com>
2025-12-28 18:54:56 +0300

    inited controller for teacher

---
+++ src/main/kotlin/com/team322/prepovedia/contoller/TeacherController.kt
@@ -0,0 +1,16 @@
+package com.team322.prepovedia.contoller
+
+
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/teachers")
+class TeacherController {
+}
---
+++ src/main/kotlin/com/team322/prepovedia/service/TeacherService.kt
@@ -0,0 +1,4 @@
+package com.team322.prepovedia.service
+
+interface TeacherService {
+}
