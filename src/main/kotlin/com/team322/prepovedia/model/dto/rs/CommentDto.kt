aee40820ffec1c93683ce98e5a394c3bb79e2e92
error404egor <egoclash129@gmail.com>
2025-12-28 20:03:30 +0300

    docker-compose +db connection

--- src/main/kotlin/com/team322/prepovedia/model/dto/rs/CommentDto.kt
+++ src/main/kotlin/com/team322/prepovedia/model/dto/rs/CommentDto.kt
@@ -3,6 +3,6 @@ package com.team322.prepovedia.model.dto.rs
 class CommentDto(
     val id: Long,
     val text: String,
-    val authorName: String,
+    val author: UserDto,
     val createdAt: java.time.LocalDateTime
 )
--- src/main/kotlin/com/team322/prepovedia/model/dto/rs/TeacherDto.kt
+++ src/main/kotlin/com/team322/prepovedia/model/dto/rs/TeacherDto.kt
@@ -1,5 +1,9 @@
 package com.team322.prepovedia.model.dto.rs
 
-class TeacherDto {
-
-}
+class TeacherDto(
+    val id: Long,
+    val fullName: String,
+    val thumbnail: String?,
+    val tags: List<TagDto>,
+    val departments: List<DepartmentDto>
+)
